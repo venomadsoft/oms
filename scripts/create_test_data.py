b592a60aa78c79f74c0e2897bb2c9cdcf3ce6637
Vritant Jain <adarshvritant@gmail.com>
2016-03-03 00:00:05 -0500

    Added creation and deletion of customers, simple gsm shades, price lists, and customer groups

--- scripts/create_test_data.py
+++ scripts/create_test_data.py
@@ -3,19 +3,36 @@ import time
 import unittest
 import swagger_client
 import random
+from random import randint
+
+api = swagger_client.ApiClient("http://localhost:3000/v2/api-docs")
+mill_api = swagger_client.MillresourceApi()
+quality_api = swagger_client.QualityresourceApi()
+sgs_api = swagger_client.SimplegsmshaderesourceApi()
+customer_api = swagger_client.CustomerresourceApi()
+pricelist_api = swagger_client.PricelistresourceApi()
+customer_group_api = swagger_client.CustomergroupresourceApi()
+
+size = 3
+fruit = ('apple', 'banana', 'cherry', 'date', 'elderberry',
+        'fig', 'grape', 'honeydew-melon', 'iceberg-lettuce',
+        'kiwi', 'lemon', 'mango', 'nut', 'orange', 'pear',
+        'quince', 'raisin', 'stawberry', 'tomato',
+        'victoria-plum', 'watermelon', 'yam', 'zucchini')
+animal = ('ant', 'bear', 'cat', 'dog', 'elephant',
+        'fox', 'goat', 'horse', 'iguana', 'jackal', 'kangaroo',
+        'lion', 'mouse', 'newt', 'oyster', 'pig', 'quail',
+        'rat', 'snake', 'tiger', 'vulture', 'wolf', 'yak',
+        'zebra')
+shade = ('red', 'yellow', 'green', 'blue', 'white', 'black')
+number = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+day = ("01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+       "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+       "21", "22", "23", "24", "25", "26", "27", "28")
+month = ("01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+       "11", "12")
 
 def random_name():
-    fruit = ('apple', 'banana', 'cherry', 'date', 'elderberry',
-            'fig', 'grape', 'honeydew-melon', 'iceberg-lettuce',
-            'kiwi', 'lemon', 'mango', 'nut', 'orange', 'pear',
-            'quince', 'raisin', 'stawverry', 'tomato',
-            'victoria-plum', 'watermelon', 'yam', 'zucchini')
-    animal = ('ant', 'bear', 'cat', 'dog', 'elephant',
-            'fox', 'goat', 'horse', 'iguana', 'jackal', 'kangaroo',
-            'lion', 'mouse', 'newt', 'oyster', 'pig', 'quail',
-            'rat', 'snake', 'tiger', 'vulture', 'wolf', 'yak',
-            'zebra')
-    number = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
     name ='-'.join([random.choice(animal),
         random.choice(fruit),
         str(random.choice(number))])
@@ -28,27 +45,69 @@ def get_code(name):
         code+=name[:1]
     return code
 
-api = swagger_client.ApiClient("http://localhost:3000/v2/api-docs")
-mill_api = swagger_client.MillresourceApi()
-quality_api = swagger_client.QualityresourceApi()
+def create_price_list():
+    pl = swagger_client.PriceList()
+    pl.wef_date_from = '-'.join(['2016',random.choice(month),random.choice(day)])
+    pl.wef_date_to = '-'.join(['2016',random.choice(month),random.choice(day)])
+    pl = pricelist_api.create_price_list_using_post(pl)
+    return pl
 
-size = 3
-mills = []
-qualities = []
-for m in range(0, size):
+def create_customer():
+    cust = swagger_client.Customer()
+    cust.name = random_name()
+    print "Creating Customer: %s" % cust.name
+    cust.code = get_code(cust.name)
+    cust = customer_api.create_customer_using_post(cust)
+    return cust
+
+def create_customer_group(mill, plist, customers):
+    group = swagger_client.CustomerGroup()
+    group.mill = mill
+    group.price_list = plist
+    group.customerss = customers
+    group.name = random_name()
+    print "Creating Customer Group: %s for Mill: %s" % (group.name, mill.name)
+    group.code = get_code(group.name)
+    group = customer_group_api.create_customer_group_using_post(group)
+    return group
+
+def create_mill():
     mill = swagger_client.Mill()
     mill.name = random_name()
-    mill.code = get_code(mill.name)
-    mills.append(mill_api.create_mill_using_post(mill))
     print "Creating Mill: %s" % mill.name
-
-i = 0
-for m in range(0, size):
-    mill = mills[m]
-    for q in range(0, size):
+    mill.code = get_code(mill.name)
+    mill = mill_api.create_mill_using_post(mill)
+    for i in range(0, size):
         quality = swagger_client.Quality()
         quality.label = random_name()
-        quality.mill = mill
-        qualities.append(quality_api.create_quality_using_post(quality))
-        i = i + 1
         print "Creating Quality: %s for Mill: %s" % (quality.label, mill.name)
+        quality.mill = mill
+        quality_api.create_quality_using_post(quality)
+    for i in range(0, size):
+        sgs = swagger_client.SimpleGsmShade()
+        sgs.shade = random.choice(shade)
+        sgs.min_gsm = randint(0, 500)
+        sgs.max_gsm = randint(0, 500)
+        sgs.mill = mill
+        print "Creating SimpleGsmShade: %s(%s-%s) for Mill: %s" % (sgs.shade, sgs.min_gsm, sgs.max_gsm, mill.name)
+        sgs_api.create_simple_gsm_shade_using_post(sgs)
+    return mill
+
+mills = []
+for i in range(0, size):
+    mills.append(create_mill())
+
+customers = []
+for i in range(0, size * 10):
+    customers.append(create_customer())
+
+plists = []
+for i in range(0, size):
+    plists.append(create_price_list())
+
+for mill in mills:
+    for plist in plists:
+        index = randint(0, size * 10 - 3)
+        create_customer_group(mill, plist, customers[index:index+3]);
+
+
--- scripts/delete_test_data.py
+++ scripts/delete_test_data.py
@@ -8,6 +8,10 @@ api = swagger_client.ApiClient("http://localhost:3000/v2/api-docs")
 mill_api = swagger_client.MillresourceApi()
 quality_api = swagger_client.QualityresourceApi()
 price_api = swagger_client.PriceresourceApi()
+sgs_api = swagger_client.SimplegsmshaderesourceApi()
+customer_api = swagger_client.CustomerresourceApi()
+customer_group_api = swagger_client.CustomergroupresourceApi()
+price_list_api = swagger_client.PricelistresourceApi()
 
 prices = price_api.get_all_prices_using_get()
 for price in prices:
@@ -19,7 +23,32 @@ for quality in qualities:
     print "Deleting Quality: %s" % quality.label
     quality_api.delete_quality_using_delete(quality.id)
 
+sgss = sgs_api.get_all_simple_gsm_shades_using_get()
+for sgs in sgss:
+    print "Deleting %s(%s-%s)" % (sgs.shade, sgs.min_gsm, sgs.max_gsm)
+    sgs_api.delete_simple_gsm_shade_using_delete(sgs.id)
+
+groups = customer_group_api.get_all_customer_groups_using_get()
+for group in groups:
+    print "Deleting Customer Group: %s" % group.name
+    customer_group_api.delete_customer_group_using_delete(group.id)
+
+plists = price_list_api.get_all_price_lists_using_get()
+for plist in plists:
+    print "Deleting Pricelist %s to %s" % (plist.wef_date_from, plist.wef_date_to)
+    price_list_api.delete_price_list_using_delete(plist.id)
+
 mills = mill_api.get_all_mills_using_get()
 for mill in mills:
     print "Deleting Mill: %s" % mill.name
     mill_api.delete_mill_using_delete(mill.id)
+
+customers = customer_api.get_all_customers_using_get()
+for customer in customers:
+    print "Deleting Customer: %s" % customer.name
+    customer_api.delete_customer_using_delete(customer.id)
+
+customers = customer_api.get_all_customers_using_get()
+for customer in customers:
+    print "Deleting Customer: %s" % customer.name
+    customer_api.delete_customer_using_delete(customer.id)
